b5e0d27d5d40912f4b45f57923552f1de5fedd2a
Nick <nicholascleary01@gmail.com>
2019-11-20 12:58:20 -0500

    Added initial files
    Initial files added

---
+++ SimonGame/SimonGame.ino
@@ -0,0 +1,13 @@
+/*Simon Game
+ * ESC 120 Group 4 Final Project
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
