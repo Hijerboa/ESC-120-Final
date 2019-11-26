c60c4e8fdd4c273aa84f0f4b32cfae10c2cb6853
Nick <nicholascleary01@gmail.com>
2019-11-26 09:20:31 -0500

    Added joystick state and joystick centered

--- SimonGame/SimonGame.ino
+++ SimonGame/SimonGame.ino
@@ -1,26 +1,14 @@
-// Core library for code-sense
-#if defined(WIRING) // Wiring specific
-#include "Wiring.h"
-#elif defined(MAPLE_IDE) // Maple specific
-#include "WProgram.h"
-#elif defined(MPIDE) // chipKIT specific
-#include "WProgram.h"
-#elif defined(DIGISPARK) // Digispark specific
-#include "Arduino.h"
-#elif defined(ENERGIA) // LaunchPad MSP430, Stellaris and Tiva, Experimeter Board FR5739 specific
-#include "Energia.h"
-#elif defined(CORE_TEENSY) // Teensy specific
-#include "WProgram.h"
-#elif defined(ARDUINO) && (ARDUINO >= 100) // Arduino 1.0 and 1.5 specific
-#include "Arduino.h"
-#elif defined(ARDUINO) && (ARDUINO < 100) // Arduino 23 specific
-#include "WProgram.h"
-#else // error
-#error Platform not defined
-#endif
-
 // Include application, user and local libraries
 #include "SPI.h"
+#include <LCD_screen.h>
+#include <LCD_screen_font.h>
+#include <LCD_utilities.h>
+#include <Screen_HX8353E.h>
+#include <Terminal12e.h>
+#include <Terminal6e.h>
+#include <Terminal8e.h>
+Screen_HX8353E myScreen;
+
 
 // Screen selection
 #define HX8353E // HX8353E K35_SPI
@@ -30,8 +18,6 @@
 Screen_K35_SPI myScreen;
 
 #elif defined(HX8353E)
-#include "Screen_HX8353E.h"
-Screen_HX8353E myScreen;
 
 #else
 #error Unknown screen
@@ -48,42 +34,89 @@ const int joyStickY = 26;
 int joyStickSelState = 0;
 int joyStickXState, joyStickYState;
 
+//LED colors
 #define RGB_BLUE_LED 37 
 #define RGB_GREEN_LED 38
 #define RGB_RED_LED 39
+
+//Buttons
+#define button1 33
+#define button2 32
+
+void startScreen(){
+  myScreen.gText(30,0, "Simon Game");
+  myScreen.gText(20, 50, "Press button 1");
+  myScreen.gText(40, 70, "to start");
+}
 
 void setup() {
   analogReadResolution(12);
   pinMode(joyStickSel, INPUT_PULLUP);
   Serial.begin(9600);
-  pinMode(RGB_BLUE_LED, OUTPUT);
+  /*pinMode(RGB_BLUE_LED, OUTPUT);
   pinMode(RGB_GREEN_LED, OUTPUT);
-  pinMode(RGB_RED_LED, OUTPUT);
- 
+  pinMode(RGB_RED_LED, OUTPUT);*/
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  myScreen.begin();
+  myScreen.setFontSize(1);
+  myScreen.setFontSolid(false);
+  fillArray();
+  startScreen();
 }
 
-void loop() {
-   myScreen.setFontSize(1);
-    myScreen.setFontSolid(false);
-    myScreen.gText(0, 60, "Hello there");
-  // put your main code here, to run repeatedly: 
+int returnDirection(){
+    joyStickXState = analogRead(joyStickX);
+    joyStickYState = analogRead(joyStickY);
+    if((joyStickYState > 3800)  && (joyStickXState > 300) && (joyStickXState < 3800)){
+      //up
+      return 1;
+    }else if((joyStickYState <300) && (joyStickXState > 300) && (joyStickXState < 3800)){
+      //down
+     return 2;
+    }else if((joyStickXState <300) && (joyStickYState > 300) && (joyStickYState < 3800)){
+      //left
+      return 3;
+    }else if((joyStickXState > 3800) && (joyStickYState > 300) && (joyStickYState < 3800)){
+      //right
+      return 4;
+    }
+}
+
+boolean joyStickCentered(){
   joyStickXState = analogRead(joyStickX);
   joyStickYState = analogRead(joyStickY);
-  Serial.print(joyStickYState);
-  if(joyStickYState < 1500){
-    digitalWrite(RGB_BLUE_LED, HIGH); 
-    delay(300); 
-    digitalWrite(RGB_BLUE_LED, LOW); 
-    delay(300);
-  }else if(joyStickYState >2500){
-    digitalWrite(RGB_GREEN_LED, HIGH); 
-    delay(300); 
-    digitalWrite(RGB_GREEN_LED, LOW); 
-    delay(300);
+  if((joyStickYState < 3800) && (joyStickYState > 300) && (joyStickXState < 3800) && (joyStickXState > 300)){
+    return true;
   }else{
-    digitalWrite(RGB_RED_LED, HIGH); 
-    delay(300); 
-    digitalWrite(RGB_RED_LED, LOW); 
-    delay(300);
+    return false;
   }
+}
+
+void fillArray(){
+  int directionsArray[250];
+  for(int i=0; i<250; i++){
+    directionsArray[i] = random(1,4);
+  }
+}
+
+int score = 0;
+
+
+boolean startButton = false;
+void loop() {
+  Serial.println(startButton);
+  if(digitalRead(button1) == 0){
+    startButton = true;
+    myScreen.clear();
+  }
+  if(startButton){ 
+    if(joyStickCentered() == true){
+      myScreen.gText(0,0,"Hello bois");
+    }else{
+      myScreen.clear();
+      myScreen.gText(0,0,"Goodbi bois"); 
+    }
+  }
+  
 }
